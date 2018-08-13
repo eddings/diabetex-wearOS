7953f57dc7b2b761dc74a9426a4872ed4f62c4a1
fgiris <fgiris@yandex.com>
2018-08-13 12:34:13 +0300

    Update min and max values for diabetes,systole,diastole and pulse

--- app/src/main/java/com/muhendis/diabetwatch/activities/ExerciseActivity.java
+++ app/src/main/java/com/muhendis/diabetwatch/activities/ExerciseActivity.java
@@ -52,7 +52,7 @@ public class ExerciseActivity extends WearableActivity implements SensorEventLis
     private LocalDBHelper mLocalDBHelper;
     private UIHelper mUIHelper;
     private FirebaseDBHelper mFirebaseDbHelper;
-    private final int diabetesMin=80,diabetesMax=350,systoleMin=100,systoleMax=180,diastoleMin=80,diastoleMax=350,pulseMin=55,pulseMax=140;
+    private final int diabetesMin=80,diabetesMax=400,systoleMin=90,systoleMax=180,diastoleMin=50,diastoleMax=150,pulseMin=50,pulseMax=145;
     private int heartRate=0,numberOfMeasurement=0;
     private ProgressDialog pdLoading;
     private EditText pulseEditText;
